56a52b1a2bf5812bc72458b89e287ef86cbec557
Akin Rigo <akinrigo7@gmail.com>
2025-04-21 17:15:51 -0300

    desafio 3 atualizado e refatorado

--- ex03/desafio3.py
+++ ex03/desafio3.py
@@ -2,47 +2,59 @@
 # que repita o fluxo até que o usuário insira as informações corretas
 
 
-#REFATORAMENTO
-
 CONSTANTE_BONUS = 1000
-
-nome_usuario = input("Digite seu nome:" )
 
 # Adicionanda duas funções que verificam se o campo esta vazio ou se o nome foi digitado errado
 
-if nome_usuario.isdigit():
-    print("Você digitou seu nome errado")
-    exit()
-elif len(nome_usuario) == 0:
-    print("O campo 'nome' não pode ficar vazio! ") 
-    exit()   
-###########################################################    
+nome_valido = False
+salario_valido = False
+bonus_valido = False
 
-try:
-    salario_usuario = float(input("Digite seu salário: "))
-    if salario_usuario < 1500:
-        print("Erro: o salário deve ser igual ou maior que R$1.500,00!")
-        exit()
-    else:
-        print(f"Seu salario é R$:{salario_usuario}.")
-except ValueError:
-    print("Erro: você deve inserir apenas números!")
-    exit()
+while not nome_valido:
+    try:
+        nome_usuario = input("Digite seu nome:" )
+
+        #verifica se o nome esta vazio
+        if len(nome_usuario) == 0:
+            raise ValueError ("O campo 'nome' não pode ficar vazio! ") 
+        # verifica se há números no nome
+        elif nome_usuario.isdigit():
+            raise ValueError("O nome não deve conter números.")
+        else:
+            print("Nome válido:", nome_usuario)
+            nome_valido = True
+    except ValueError as e:
+        print(e)
+
+###########################################################  
+#  
+
+
+while salario_valido is not True:
+    try:
+        salario_usuario = float(input("Digite seu salário: "))
+        if salario_usuario < 1500:
+            print("Erro: o salário deve ser igual ou maior que R$1.500,00!")
+            salario_valido = True
+    except ValueError:
+        print("Erro: você deve inserir apenas números!")
 
 
 #####################################################
 
-try:
 
-    bonus_usuario = float(input("Digite o seu bônus: "))
+while bonus_valido is not True:
 
-    valor_bonus = CONSTANTE_BONUS + salario_usuario * bonus_usuario
+    try:
 
-    if bonus_usuario < 300:
-        print("Erro: o valor do bônus deve ser igual ou maior que R$300,00")
-    else:    
-        print(f"O usuário {nome_usuario} possui o bônus de {valor_bonus}.")
-    
-except ValueError:
-  print("Erro: você deve inserir apenas números!")
-exit()
+        bonus_usuario = float(input("Digite o seu bônus: "))
+
+        valor_bonus = CONSTANTE_BONUS + salario_usuario * bonus_usuario
+
+        if bonus_usuario < 300:
+            print("Erro: o valor do bônus deve ser igual ou maior que R$300,00")
+        else:    
+            print(f"O usuário {nome_usuario} possui o bônus de {valor_bonus}.")
+        
+    except ValueError:
+        print("Erro: você deve inserir apenas números!")
