333bb9bfb2a2b0086722f426dd55debcbd5460e7
Akin Rigo <akinrigo7@gmail.com>
2025-04-23 20:36:47 -0300

    treino de lista e dicionários

---
+++ ex04/exercicio4.py
@@ -0,0 +1,9 @@
+#Crie uma lista com os números de 1 a 10 e use um loop para imprimir cada número elevado ao quadrado
+def numero_lista() -> list[int]:
+    return(range(1,11))
+
+#função para elevar ao quadrado
+
+for numero_lista in range[1,11]:
+    quadrado = numero_lista ** 2
+print(f"{numero_lista}² = `{quadrado}")
---
+++ ex04/exercicio5.py
@@ -0,0 +1,5 @@
+#Da lista ["Python", "Java", "C++", "JavaScript"], remova o item "C++" e adicione "Ruby"
+language = ["Python", "Java", "C++", "JavaScript"]
+language.remove ("C++")
+language.insert(2,"Ruby")
+print(language)
